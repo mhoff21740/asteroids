aba0f1ada28caa46cf3ae8c7b27cc3b17aa3c818
mhoff21740 <marshall.hoffmaster@gmail.com>
2025-06-13 08:45:03 -0400

    Added gameloop skelton and variables to main.py, CH2:L2

--- main.py
+++ main.py
@@ -3,9 +3,27 @@
 # throughout this file
 import pygame
 
-def main():
-    print("Starting Asteroids!")
 
+#import constant variables from constants.py into main.py
+from constants import * 
+
+
+def main():
+    pygame.init() # initializes the pygame library
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+             pygame.quit()
+        # exit the game if the window is closed
+        pygame.Surface.fill(screen, (0,0,0))  # fill the screen with black color
+        pygame.display.flip()
+        # this creates a window with the specified width and height
 
 
 
