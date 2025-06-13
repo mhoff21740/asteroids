e8d7dd784c88277542bd03bd40a394d4c90e8a8b
mhoff21740 <marshall.hoffmaster@gmail.com>
2025-06-13 14:11:32 -0400

    progress as of 1410 6/13, Ch3 L3

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import pygame
 
 #import constant variables from constants.py into main.py
 from constants import * 
+from circleshape import *
+from player import *
+from asteroidfield import *
 
 
 def main():
@@ -16,14 +19,47 @@ def main():
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+clock = pygame.time.Clock() # set the frame rate to 60 frames per second
+dt = 0
+
+
+asteroids = pygame.sprite.Group()
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+AsteroidField.containers = (updatable)
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+asteroid_field = AsteroidField()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              pygame.quit()
         # exit the game if the window is closed
-        pygame.Surface.fill(screen, (0,0,0))  # fill the screen with black color
-        pygame.display.flip()
-        # this creates a window with the specified width and height
+    pygame.Surface.fill(screen, (0,0,0))  # fill the screen with black color
+    updatable.update(dt)
+    for asteroid in asteroids:
+        if asteroid.collision(player):
+            print("Game Over!")
+            pygame.quit()
+            raise SystemExit 
+    for player in drawable:
+        player.draw(screen)
+    pygame.display.flip()
+    
+   
+   
+   
+   
+   
+   
+    # this creates a window with the specified width and height
+    dt = clock.tick(60) / 1000.0
+        
 
 
 
