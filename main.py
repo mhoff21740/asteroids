4ba25799f7446c4d90614836bc79c980ff14ef10
mhoff21740 <marshall.hoffmaster@gmail.com>
2025-06-13 06:39:29 -0400

    progress on game as of 6/13/25 0639, added main.py file

---
+++ main.py
@@ -0,0 +1,17 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    # this initializes the pygame library
